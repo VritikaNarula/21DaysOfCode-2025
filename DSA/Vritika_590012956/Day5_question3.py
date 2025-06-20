d308a16324fc96186d6d4c28e74679ec32462990
VritikaNarula <vritikanarula31@gmail.com>
2025-06-20 22:00:04 +0530

    Day5_question3.py

--- DSA/Vritika_590012956/Day5_question3.py
+++ DSA/Vritika_590012956/Day5_question3.py
@@ -3,6 +3,6 @@ nums = [int(num) for num in nums]
 
 reversed_nums = list(reversed(nums))
 if nums == reversed_nums:
-    print("The array is a palindrome.")
+    print("True,The array is a palindrome.")
 else:
-    print("The array is not a palindrome.")
+    print("False,The array is not a palindrome.")
